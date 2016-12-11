d8ba7731ee00bdf61671cf4357f5d18ce6dd275e
Goichi Hirakawa <gooichi@gyazsquare.com>
2016-12-12 00:22:25 +0900

    Update version to v1.0.2

--- flycheck-swift3.el
+++ flycheck-swift3.el
@@ -4,7 +4,7 @@
 
 ;; Author: Goichi Hirakawa <gooichi@gyazsquare.com>
 ;; URL: https://github.com/GyazSquare/flycheck-swift3
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Keywords: convenience, languages, tools
 ;; Package-Requires: ((emacs "24.4") (flycheck "26"))
 
